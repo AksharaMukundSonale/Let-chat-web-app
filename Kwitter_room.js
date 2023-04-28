bd52db5dcc0255d08d10e7f837f9fe698d137ff0
AksharaMukundSonale <91076766+AksharaMukundSonale@users.noreply.github.com>
2023-04-28 13:48:27 +0530

    Add files via upload

---
+++ Kwitter_room.js
@@ -0,0 +1,14 @@
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyD-0i_r6RspPsO5y2l2kx1w2aQT2SV14Y8",
+    authDomain: "lets-chat-web-app-ef997.firebaseapp.com",
+    projectId: "lets-chat-web-app-ef997",
+    storageBucket: "lets-chat-web-app-ef997.appspot.com",
+    messagingSenderId: "221093709792",
+    appId: "1:221093709792:web:771124a0370f5272c00d85",
+    measurementId: "G-85NEQJ8BY5"
+  };
+  
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
