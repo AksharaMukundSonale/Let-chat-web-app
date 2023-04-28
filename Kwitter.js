fa8708a9ae600941401e846aac24ae5a3b25f7bd
AksharaMukundSonale <91076766+AksharaMukundSonale@users.noreply.github.com>
2023-04-28 13:50:16 +0530

    Add files via upload

--- Kwitter.js
+++ Kwitter.js
@@ -1,5 +1,5 @@
 function addUser (){
     user_name = document.getElementById("user_name").value;
     localStorage.setItem("User is", user_name);
-    window.location = "kwitter_room.html";
+    window.location = "Kwitter_room.html";
 }
